3e49456bf97553103ad7d733fcd68f1abec38c7c
Prafulla Giri <pratheblackdiamond@gmail.com>
2020-08-30 00:33:49 +0545

    Fix and update slowmovideo package definition.
    Might need a little bit of polishing up, though.

--- slowmovideo.scm
+++ slowmovideo.scm
@@ -1,45 +1,60 @@
 (define-module (slowmovideo)
   #:use-module (guix packages)
-  #:use-module (gnu packages python)
-  #:use-module (gnu packages perl)
+  #:use-module (guix licenses)
   #:use-module (gnu packages qt)
-  #:use-module (gnu packages video)
-  #:use-module (gnu packages image-processing)
   #:use-module (guix git-download)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages python)
   #:use-module (guix build-system cmake)
-  #:use-module (guix licenses))
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages image-processing))
 
 (define-public slowmovideo
   (package
-    (name "slowmovideo")
-    (version "latest-git")
-    (source
-      (origin
-  	  (method git-fetch)
-  	  (uri (git-reference
-  	       (url "https://github.com/slowmoVideo/slowmoVideo.git")
-  		   (commit "6ed913bbf06758f10a41e44d8a8292ae555f486c")))
-  	  (file-name (git-file-name name version))
-  	  (sha256
-  	    (base32
-  		  "10w50cj05hbskfcpfzhmk4263vd8baks7k6y5iqrf78q43xx544n"))))
-    (build-system cmake-build-system)
-	(arguments
-	  ;;<reepca> peanutbutterandc: from what I can tell they have a somewhat unusual build system for that project, as instead of the usual top-level invocation of cmake, they want you to run it in one of the two given subdirectories ("slowmoVideo" or "V3D"). So you'll need to replace the cmake-build-system's invocation of cmake (which currently passes "../source") with one that passes "../source/slowmoVideo". Alternatively, you could add a phase after
-	  ;;<reepca> 'unpack which chdir's into slowmoVideo (source -> source/slowmoVideo) and assume that there's no existing directory named "build" in source, which is probably a safe assumption.
-	  ;;<reepca> tl;dr version: (modify-phases %standard-phases (add-after 'unpack 'select-subproject (lambda _ (chdir "slowmoVideo"))))
-	  `(#:tests? #f #:phases
-	    (modify-phases %standard-phases
-		  (add-after 'unpack 'select-subproject
-		    (lambda _ (chdir "src"))))))
-	(inputs
-	  `(("ffmpeg" ,ffmpeg)
-	    ("qtbase" ,qtbase)
-	    ("qtscript" ,qtscript)
-	    ("opencv" ,opencv)))
-    (synopsis "Create slow-motion videos from your footage")
-    (description synopsis)
-    (home-page "http://slowmovideo.granjow.net/")
-    (license gpl3)))
-
-;; guix environment --pure
+   (name "slowmovideo")
+   (version "0.6")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url "https://github.com/slowmoVideo/slowmoVideo.git")
+	   (commit (string-append "v" version))))
+     (sha256
+      (base32
+       "0wgka5g248m17qmzdhzql17z6a046wxqv8zc6rar762f6g7qh3dr"))
+     (file-name (git-file-name name version))))
+   (build-system cmake-build-system)
+   (arguments
+    `(#:tests? #f ;; there are no tests
+      #:phases
+      (modify-phases %standard-phases
+		     (add-after 'unpack 'add-submodules
+				(lambda _
+				  (copy-recursively (assoc-ref %build-inputs "libsvflow")
+						    "src/lib/libsvflow"))))))
+   (inputs
+    `(("ffmpeg" ,ffmpeg)
+      ("qtbase" ,qtbase)
+      ("qtscript" ,qtscript)
+      ("opencv" ,opencv)
+      ("libsvflow"
+       ;; This is a super-clever way to deal with git submodules
+       ;; I was unable to deal with submodules using (git-reference
+       ;; (recursive? #t). Partly because of git protocol used. This
+       ;; clever method was suggested by leoprikler, who did this
+       ;; same thing with `ppsspp`.
+       ,(let ((commit "7c31a0bf9467e774442473e8b951b09fe6eb1b9f"))
+	  (origin
+	   (method git-fetch)
+	   (uri (git-reference
+		 (url "https://github.com/slowmoVideo/libsvflow.git")
+		 (commit commit)))
+	   (sha256
+	    (base32 "05q125350vg3q812p5ph0p14bbbicpw1dmisrxja4rvbi0lnixq1"))
+	   (file-name (git-file-name "libsvflow" commit)))))))
+   (native-inputs
+    `(("python-minimal" ,python-minimal)))
+   (synopsis "Create slow-motion videos from your footage")
+   (description "SlowmoVideo is a tool that uses optical flow for generating slow-motion videos.")
+   (home-page "http://slowmovideo.granjow.net/")
+   (license gpl3)))
