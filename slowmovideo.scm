349839865674b277738ff8eaf5fb70c1a4bfd9c6
Prafulla Giri <pratheblackdiamond@gmail.com>
2020-08-30 11:22:37 +0545

    * slowmovideo.scm: Fix indentation

--- slowmovideo.scm
+++ slowmovideo.scm
@@ -17,8 +17,8 @@
     (origin
      (method git-fetch)
      (uri (git-reference
-	   (url "https://github.com/slowmoVideo/slowmoVideo.git")
-	   (commit (string-append "v" version))))
+           (url "https://github.com/slowmoVideo/slowmoVideo.git")
+           (commit (string-append "v" version))))
      (sha256
       (base32
        "0wgka5g248m17qmzdhzql17z6a046wxqv8zc6rar762f6g7qh3dr"))
@@ -28,10 +28,10 @@
     `(#:tests? #f ;; there are no tests
       #:phases
       (modify-phases %standard-phases
-		     (add-after 'unpack 'add-submodules
-				(lambda _
-				  (copy-recursively (assoc-ref %build-inputs "libsvflow")
-						    "src/lib/libsvflow"))))))
+                     (add-after 'unpack 'add-submodules
+                                (lambda _
+                                  (copy-recursively (assoc-ref %build-inputs "libsvflow")
+                                                    "src/lib/libsvflow")))
    (inputs
     `(("ffmpeg" ,ffmpeg)
       ("qtbase" ,qtbase)
@@ -44,14 +44,14 @@
        ;; clever method was suggested by leoprikler, who did this
        ;; same thing with `ppsspp`.
        ,(let ((commit "7c31a0bf9467e774442473e8b951b09fe6eb1b9f"))
-	  (origin
-	   (method git-fetch)
-	   (uri (git-reference
-		 (url "https://github.com/slowmoVideo/libsvflow.git")
-		 (commit commit)))
-	   (sha256
-	    (base32 "05q125350vg3q812p5ph0p14bbbicpw1dmisrxja4rvbi0lnixq1"))
-	   (file-name (git-file-name "libsvflow" commit)))))))
+          (origin
+           (method git-fetch)
+           (uri (git-reference
+                 (url "https://github.com/slowmoVideo/libsvflow.git")
+                 (commit commit)))
+           (sha256
+            (base32 "05q125350vg3q812p5ph0p14bbbicpw1dmisrxja4rvbi0lnixq1"))
+           (file-name (git-file-name "libsvflow" commit)))))))
    (native-inputs
     `(("python-minimal" ,python-minimal)))
    (synopsis "Create slow-motion videos from your footage")
